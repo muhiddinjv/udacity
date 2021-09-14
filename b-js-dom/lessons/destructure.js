61cf89244a0f0715b34ceedc36b67a93c940e514
Emuallim <muhiddin001@gmail.com>
2021-09-14 22:21:43 +0500

    add shorthand for destructure & obj literal

---
+++ b-js-dom/lessons/destructure.js
@@ -0,0 +1,49 @@
+// DESTRUCTURE ARRAY -------------
+const point = [10, 25, -34];
+
+//Looong
+const x = point[0];
+const y = point[1];
+const z = point[2];
+
+// short
+const [x, y, z] = point;
+console.log(x, y, z);
+
+
+// DESTRUCTURE OBJECT ------------
+const gemstone = {
+  type: 'quartz',
+  color: 'rose',
+  carat: 21.29
+};
+
+//Looong
+const type = gemstone.type;
+const color = gemstone.color;
+const carat = gemstone.carat;
+
+// short
+const {type, color, carat} = gemstone
+console.log(type, color, carat);
+
+
+// Object literal -------------
+let type = 'quartz';
+let color = 'rose';
+let carat = 21.29;
+
+//looong
+const gemstone = {
+  type: type,
+  color: color,
+  carat: carat,
+  calculateWorth: function() {
+  // will calculate worth of gemstone based on type, color, and carat
+  }
+};
+
+//short if properties name = variables name
+const gemstone = {type, color, carat, calculateWorth() { }};
+console.log(gemstone);
+// Prints: Object {type: "quartz", color: "rose", carat: 21.29}
