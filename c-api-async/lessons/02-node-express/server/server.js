bf59aab99fb600b8ac01466728ca1054915f8ccc
Emuallim <muhiddin001@gmail.com>
2021-09-24 21:47:42 +0500

    put files into folders

--- c-api-async/lessons/02-node-express/server/server.js
+++ c-api-async/lessons/02-node-express/server/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 /* Dependencies */
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -15,3 +15,12 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('../client'));
+
+const port = 8000;
+const server = app.listen(port, ()=>{console.log(`server running on local host: ${port}`)});
+
+// Spin up the server
+app.get("/",(req, res)=>{res.send('hello hi')})
