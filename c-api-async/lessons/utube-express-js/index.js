c299e06cdd5e7dc92fb6e5c28a66ced987727334
Emuallim <muhiddin001@gmail.com>
2021-09-24 23:13:08 +0500

    update nodemon config

--- c-api-async/lessons/utube-express-js/index.js
+++ c-api-async/lessons/utube-express-js/index.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.get('/',(req, res)=>{
-  res.send('<h1>Hello Muhiddin</h1>')
+  res.send('<h1>Hello me</h1>')
 })
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
+app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
