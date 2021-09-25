29e084e82e78e6d19f1fc07dd7f3002303f2fce6
Emuallim <muhiddin001@gmail.com>
2021-09-26 00:55:08 +0500

    feat: add api routes

--- c-api-async/lessons/utube-express-js/index.js
+++ c-api-async/lessons/utube-express-js/index.js
@@ -1,18 +1,18 @@
 // import
 const express = require("express");
 const path = require("path");
-const members = require("./Members");
-const logger = require("./middleware/logger");
+// const logger = require("./middleware/logger");
 
 const app = express();
 
 //init middleware
 // app.use(logger);
 
-// gets all members
-app.get("/api/members", (req, res) => res.json(members));
-
+// Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
