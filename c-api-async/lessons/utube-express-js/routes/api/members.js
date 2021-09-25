72f2a9b3f4b35e58734210950f543ba14eb3993d
Emuallim <muhiddin001@gmail.com>
2021-09-26 01:31:43 +0500

    feat: add update & delete member logic

--- c-api-async/lessons/utube-express-js/routes/api/members.js
+++ c-api-async/lessons/utube-express-js/routes/api/members.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const members = require("../../Members");
 
 // Get all members
-router.get('/', (req, res) => res.json(members));
+router.get("/", (req, res) => res.json(members));
 
 // Get single member
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const found = members.some((member) => member.id === parseInt(req.params.id));
 
   if (found) {
@@ -17,20 +17,53 @@ router.get('/:id', (req, res) => {
 });
 
 // Create member
-router.post('/', (req, res)=>{
+router.post("/", (req, res) => {
   const newMember = {
     id: uuid.v4(),
     name: req.body.name,
     email: req.body.email,
-    status: 'active'
+    status: "active",
+  };
+
+  if (!newMember.name || !newMember.email) {
+    return res.status(400).json({ msg: "Please include a name & email" });
   }
-
-  if(!newMember.name || !newMember.email){
-    return res.status(400).json({msg: 'Please include a name & email'});
-  } 
-
   members.push(newMember);
-  
+});
+
+// Update member
+router.put("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    const updMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updMember.name ? updMember.name : member.name;
+        member.email = updMember.email ? updMember.email : member.email;
+
+        res.json({ msg: "Member updated", member });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+// Delete member
+router.delete("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: "Member deleted",
+      members: members.filter(
+        (member) => member.id === parseInt(req.params.id)
+      ),
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 module.exports = router;
