780ceea2a7210f89564fa2adeec9da20fc992a06
Emuallim <muhiddin001@gmail.com>
2021-10-03 10:46:03 +0500

    feat: add app.js

--- c-api-async/lessons/02-node-express/server/server.js
+++ c-api-async/lessons/02-node-express/server/server.js
@@ -1,14 +1,12 @@
 // Express to run server and routes
 const express = require('express');
-
 // Start up an instance of app
 const app = express();
 
 /* Dependencies */
 const bodyParser = require('body-parser');
-
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+//Here we are configuring express to use body-parser as middleware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -19,8 +17,20 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('../client'));
 
-const port = 8000;
-const server = app.listen(port, ()=>{console.log(`server running on local host: ${port}`)});
+const port = 5000;
+const server = app.listen(port, ()=>{console.log(`Running on localhost: ${port}`)});
 
 // Spin up the server
-app.get("/",(req, res)=>{res.send('hello hi')})
+// app.get("/",(req, res)=>{res.send('hello hi')})
+
+
+// Here is how you could setup a basic POST route in the server side code.
+// First, Create an array to hold data:
+const data = [];
+
+//Then, create post() with a url path and a callback function:
+app.post('/addMovie', (req, res)=>{
+    console.log(req.body)
+    // In the callback function, add the data received from request.body. This is the key piece of information we are interested in from that long stretch of data we saw previously that the request (req) argument returns.
+    data.push(req.body)
+})
