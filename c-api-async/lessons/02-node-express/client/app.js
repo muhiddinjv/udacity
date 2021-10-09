8042455587cfa00ed74dc3fe3d0b0339fbb02892
Emuallim <muhiddin001@gmail.com>
2021-10-09 23:27:29 +0500

    refactor: modify and rename files

--- c-api-async/lessons/02-node-express/client/app.js
+++ c-api-async/lessons/02-node-express/client/app.js
@@ -1,24 +1,85 @@
-const postData = async ( url = '', data = {})=>{
-  console.log(data);
-    const response = await fetch(url, {
-    method: 'POST', // GET, POST, PUT, DELETE, etc
-    credentials: 'same-origin',// include "same-origin", omit
-    headers: {
-        'Content-Type': 'application/json',
-    },
-   // Body data type must match "Content-Type" header        
-    body: JSON.stringify(data), 
-  });
+// let baseURL = 'http://api.animalinfo.org/data/?animal=';
+// let apiKey = '&appid=9f15e45060';
 
-    try {
-      const newData = await response.json();
-      console.log(newData);
-      return newData;
-    }catch(error) {
-      //appropriately handle the error
-    console.log("error", error);
-    }
+// document.getElementById('generate').addEventListener('click', performAction);
+
+// function performAction(e){
+//   const newAnimal =  document.getElementById('animal').value;
+//   const favFact =  document.getElementById('favorite').value;
+
+//   getAnimal('/animalData',)
+//   // New Syntax!
+//   .then(function(data){
+//     // Add data
+//     console.log(data);
+//     postData('/addAnimal', {animal:data.animal, fact: data.fact, fav:favFact} );
+//   })
+//   .then(
+//     updateUI()
+//   )
+// }
+
+// const updateUI = async () => {
+//   const request = await fetch('/all');
+//   try{
+//     const allData = await request.json();
+//     document.getElementById('animalName').innerHTML = allData[0].animal;
+//     document.getElementById('animalFact').innerHTML = allData[0].facts;
+//     document.getElementById('animalFav').innerHTML = allData[0].fav;
+
+//   }catch(error){
+//     console.log("error", error);
+//   }
+// }
+
+
+// Async POST
+const postData = async ( url = '', data = {})=>{
+
+  const response = await fetch(url, {
+  method: 'POST', 
+  credentials: 'same-origin', 
+  headers: {
+      'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data), // body data type must match "Content-Type" header        
+});
+
+  try {
+    const newData = await response.json();
+    return newData;
+  }catch(error) {
+  console.log("error", error);
+  }
+};
+
+// Async GET
+const retrieveData = async (url='') =>{ 
+const request = await fetch(url);
+try {
+// Transform into JSON
+const allData = await request.json()
+}
+catch(error) {
+  console.log("error", error);
+  // appropriately handle the error
+}
+};
+
+
+// In this exercise, you will bring it all together by chaining async requests.
+// 1) In the file getPost.js, write a function that chains together the two async functions you have previously written, so that you make a POST request to the /animal route, and then retrieve the data with a GET request to the /all path.
+
+// You should pass in a data object of your favorite animal as the second argument for the POST request.
+
+// Call your function as the last line of code in the file named getPost.js
+// TODO-Chain your async functions to post an animal then GET the resulting data
+function postGet(){
+  postData('/animal', {fav:'lion'})
+    .then(function(data){
+      retrieveData('/all')
+    })
 }
 
-postData('/addMovie', {movie: ' the matrix', score:5});
-postData('/addMovie', {movie: ' avatar', score:4});
+// TODO-Call the chained function
+postGet()
--- c-api-async/lessons/02-node-express/server/server.js
+++ c-api-async/lessons/02-node-express/server/server.js
@@ -1,37 +1,50 @@
-// Express to run server and routes
+/* Empty JS object to act as endpoint for all routes */
+projectData = {};
+
+/* Express to run server and routes */
 const express = require('express');
-// Start up an instance of app
+
+/* Start up an instance of app */
 const app = express();
 
 /* Dependencies */
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 /* Middleware*/
-//Here we are configuring express to use body-parser as middleware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
 
-// Initialize the main project folder
-app.use(express.static('../client'));
+/* Initialize the main project folder*/
+app.use(express.static('website'));
 
 const port = 5000;
-const server = app.listen(port, ()=>{console.log(`Running on localhost: ${port}`)});
+/* Spin up the server*/
+const server = app.listen(port, listening);
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
 
-// Spin up the server
-// app.get("/",(req, res)=>{res.send('hello hi')})
+// GET route
+app.get('/all', sendData);
 
+function sendData (request, response) {
+  response.send(projectData);
+};
 
-// MOVIE EXAMPLE
-// Here is how you could setup a basic POST route in the server side code.
-// First, Create an array to hold data:
+// POST route
+app.post('/add', callBack);
+
+function callBack(req,res){
+  res.send('POST received');
+};
+
+// POST an animal
 const data = [];
-//Then, create post() with a url path and a callback function:
-app.post('/addMovie', addMovie );
 
-function addMovie (req, res){
-  // In the callback function, add the data received from request.body. This is the key piece of information we are interested in from that long stretch of data we saw previously that the request (req) argument returns.
-  data.push(req.body);
-  console.log(data);
+app.post('/animal', addAnimal);
+
+function addAnimal (req,res){
+    data.push(req.body);
 };
