7ca5d666a75153ddc306cc3c34d7a82973c3dd04
Emuallim <muhiddin001@gmail.com>
2021-10-09 23:59:22 +0500

    project setup

--- c-api-async/p3-weather/server.js
+++ c-api-async/p3-weather/server.js
@@ -1,36 +1,36 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+console.log("hi");
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
+/* Dependencies & Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
-// Setup empty JS object to act as endpoint for all routes
+const port = 5000;
+
+const server = app.listen(port, () => {
+  console.log(server);
+  console.log(`running on localhost: ${port}`);
+});
+
 // Express to run server and routes
 
-// Start up an instance of app
-
-/* Dependencies */
-/* Middleware*/
-
-//Here we are configuring express to use body-parser as middle-ware.
-// Cors for cross origin allowance
-
-// Initialize the main project folder
-
-// Spin up the server
 // Callback to debug
 
 // Initialize all route with a callback function
@@ -38,3 +38,8 @@ app.use(express.static('website'));
 // Callback function to complete GET '/all'
 
 // Post Route
+app.post('/add', callBack);
+
+function callBack(req,res){
+  res.send('POST received');
+};
