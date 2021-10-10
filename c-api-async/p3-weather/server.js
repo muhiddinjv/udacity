a34dd67bb497ed6eabca92b83fe245a45dbb9271
Emuallim <muhiddin001@gmail.com>
2021-10-11 00:08:21 +0500

    feat: add style to weather app

--- c-api-async/p3-weather/server.js
+++ c-api-async/p3-weather/server.js
@@ -1,6 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-console.log("hi");
 
 // Require Express to run server and routes
 const express = require("express");
@@ -25,7 +24,6 @@ app.use(express.static("website"));
 const port = 5000;
 
 const server = app.listen(port, () => {
-  console.log(server);
   console.log(`running on localhost: ${port}`);
 });
 
