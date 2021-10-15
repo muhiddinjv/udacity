a48afe6b21e91b4376579e8fbc7b89902fab10aa
Emuallim <muhiddin001@gmail.com>
2021-10-16 01:29:53 +0500

    refactor: update some code

--- c-api-async/lessons/02-node-express/client/app.js
+++ c-api-async/lessons/02-node-express/client/app.js
@@ -1,85 +1,87 @@
 // let baseURL = 'http://api.animalinfo.org/data/?animal=';
 // let apiKey = '&appid=9f15e45060';
 
-// document.getElementById('generate').addEventListener('click', performAction);
+document.getElementById('generate').addEventListener('click', performAction);
+const city =  document.getElementById('animal');
 
-// function performAction(e){
-//   const newAnimal =  document.getElementById('animal').value;
-//   const favFact =  document.getElementById('favorite').value;
+// Personal API Key for OpenWeatherMap API
+const api = "ad8a4d7750ce20473c84cde66d6c7ab4";
+//d0889ce843a11270e6749177a5118aec -- my 2nd api
 
-//   getAnimal('/animalData',)
-//   // New Syntax!
-//   .then(function(data){
-//     // Add data
-//     console.log(data);
-//     postData('/addAnimal', {animal:data.animal, fact: data.fact, fav:favFact} );
-//   })
-//   .then(
-//     updateUI()
-//   )
+// Event listener to add function to existing HTML DOM element
+// generate.addEventListener("click", getWeather);
+
+
+
+// async function fetchData(city) {
+//   const response = await fetch(
+//     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`
+//   );
+//   const newData = await response.json();
+//   return newData;
 // }
 
-// const updateUI = async () => {
-//   const request = await fetch('/all');
-//   try{
-//     const allData = await request.json();
-//     document.getElementById('animalName').innerHTML = allData[0].animal;
-//     document.getElementById('animalFact').innerHTML = allData[0].facts;
-//     document.getElementById('animalFav').innerHTML = allData[0].fav;
+function performAction() {
+  const fav =  document.getElementById('favorite').value;
+  
+  getAnimal()
+  //new syntax
+  .then(function(data){
+    // Add data
+    // console.log(data);
+    postData('/addAnimal', {animal: data.name, fact: data.timezone, fav: fav} );
+    // we can do this because of async!
+    updateUI()
+  })
+};
 
-//   }catch(error){
-//     console.log("error", error);
-//   }
-// }
+const getAnimal = async () => {
+  // const request = await fetch(url+api+animal);
+  const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${api}&units=metric`
+      );
+  // const res = await fetchData(city);
+  // const res = await fetch(url);
+  try{
+    const data = await res.json();
+    console.log(data);
+    return data;
+  }catch(error){
+    console.log("error", error);
+  }
 
+}
 
 // Async POST
 const postData = async ( url = '', data = {})=>{
+  console.log(data);
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+  
+    try {
+      const newData = await response.json();
+      console.log(newData);
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+  };
 
-  const response = await fetch(url, {
-  method: 'POST', 
-  credentials: 'same-origin', 
-  headers: {
-      'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data), // body data type must match "Content-Type" header        
-});
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try{
+    const allData = await request.json();
+    document.getElementById('animalName').innerHTML = allData[0].name;
+    document.getElementById('animalFact').innerHTML = allData[0].timezone;
+    document.getElementById('animalFav').innerHTML = allData[0].fav;
 
-  try {
-    const newData = await response.json();
-    return newData;
-  }catch(error) {
-  console.log("error", error);
+  }catch(error){
+    console.log("error", error);
   }
-};
-
-// Async GET
-const retrieveData = async (url='') =>{ 
-const request = await fetch(url);
-try {
-// Transform into JSON
-const allData = await request.json()
 }
-catch(error) {
-  console.log("error", error);
-  // appropriately handle the error
-}
-};
-
-
-// In this exercise, you will bring it all together by chaining async requests.
-// 1) In the file getPost.js, write a function that chains together the two async functions you have previously written, so that you make a POST request to the /animal route, and then retrieve the data with a GET request to the /all path.
-
-// You should pass in a data object of your favorite animal as the second argument for the POST request.
-
-// Call your function as the last line of code in the file named getPost.js
-// TODO-Chain your async functions to post an animal then GET the resulting data
-function postGet(){
-  postData('/animal', {fav:'lion'})
-    .then(function(data){
-      retrieveData('/all')
-    })
-}
-
-// TODO-Call the chained function
-postGet()
