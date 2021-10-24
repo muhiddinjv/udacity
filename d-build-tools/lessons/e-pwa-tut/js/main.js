6bb18dffc3480156b22f546f663943632db93dc7
Emuallim <muhiddin001@gmail.com>
2021-10-24 14:41:40 +0500

    service workers

--- d-build-tools/lessons/e-pwa-tut/js/main.js
+++ d-build-tools/lessons/e-pwa-tut/js/main.js
@@ -28,3 +28,19 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   editor.setContent(defaultText);
 });
+
+import swURL from 'sw:../service-worker.js';
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      const reg = await navigator.serviceWorker.register(swURL);
+      console.log('Service worker registered! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registration failed: ', err);
+    }
+  });
+}
